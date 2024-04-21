28d48c41abfd94d42e2b72bc7976fcadfa9889b3
Kevin Barnett <kevin.brnett@gmail.com>
2024-04-21 16:19:14 -0600

    streamlit app

--- streamlit_main.py
+++ streamlit_main.py
@@ -105,12 +105,9 @@ def make_prediction(distance, pressure, hrv, blood_sugar, spo2, accelerometer, d
     explainer = shap.TreeExplainer(rf_classifier)
     shap_values = explainer.shap_values(X_test_df)
     
-    # Determine the most important feature
-    most_important_feature_index = np.argmax(np.abs(shap_values))
-    most_important_feature = X_test.columns[most_important_feature_index]
     
     # Return prediction labels and confidence scores
-    return prediction_label, confidence_scores, most_important_feature
+    return prediction_label, confidence_scores, shap_values
                             
     
 ## Load data
@@ -212,7 +209,7 @@ if 'accelerometer_slider' not in st.session_state:
 
 
 if st.sidebar.button('Run Prediction'):
-    prediction_label, confidence_scores, most_important_feature = make_prediction(distance, pressure, hrv,
+    prediction_label, confidence_scores, shap_values = make_prediction(distance, pressure, hrv,
                                                           blood_sugar, spo2, accelerometer, df)  
     max_confidence_label = max(confidence_scores, key=confidence_scores.get)
     max_confidence_score = confidence_scores[max_confidence_label]
@@ -405,7 +402,24 @@ with col3:
         if 'predict_button_clicked' in st.session_state:
             
             ## Show output
-            prediction_placeholder.write(most_important_feature)
+            prediction_placeholder.write('tbd')
+            
+            
+# Assuming you want to analyze the first (and only) instance in the input
+instance_index = 0
+
+# Determine the most important feature for each class
+important_features = {}
+for class_index in range(3):  # Adjust based on the number of classes
+    class_shap_values = shap_values[class_index][instance_index]
+    most_important_feature_index = np.argmax(np.abs(class_shap_values))
+    most_important_feature = X_test_df.columns[most_important_feature_index]
+    important_features[f'Class {class_index}'] = most_important_feature
+
+# Display the results
+st.write("Most important features for each class:")
+for class_label, feature in important_features.items():
+    st.write(f"{class_label}: {feature}")
             
 
 
