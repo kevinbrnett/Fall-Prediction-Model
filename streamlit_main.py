0c1df2e81a93ce8ddc4ccd872b4bc6396d9e45e3
Kevin Barnett <kevin.brnett@gmail.com>
2024-04-21 19:24:29 -0600

    streamlit app

--- streamlit_main.py
+++ streamlit_main.py
@@ -87,27 +87,28 @@ def make_prediction(distance, pressure, hrv, blood_sugar, spo2, accelerometer, d
     ## Calculate SHAP values
     # Feature names extracted from the training set
     feature_names = X_train.columns
-
+    
     # Transform the test set
     X_test_df = pd.DataFrame(rf_pipe[:-1].transform(X_test),
                              columns=feature_names,
                              index=X_test.index)
+    
+    instance_index = 0
+    instance_data = X_test_df.iloc[[instance_index]]
+    predictions = rf_pipe.predict(instance_data)
+    predicted_class_index = np.argmax(predictions, axis=1)[0]
+               
 
     # Access the RandomForestClassifier from the pipeline
     rf_classifier = rf_pipe.named_steps['randomforestclassifier']
 
-    # Transform the test set
-    X_test_df = pd.DataFrame(rf_pipe[:-1].transform(X_test),
-                             columns=feature_names,
-                             index=X_test.index)
-
     # Create a SHAP explainer
     explainer = shap.TreeExplainer(rf_classifier)
-    shap_values = explainer.shap_values(X_test_df)
+    shap_values = explainer.shap_values(instance_data)
     
     
     # Return prediction labels and confidence scores
-    return prediction_label, confidence_scores, shap_values
+    return prediction_label, confidence_scores, shap_values, X_test_df
                             
     
 ## Load data
@@ -209,7 +210,7 @@ if 'accelerometer_slider' not in st.session_state:
 
 
 if st.sidebar.button('Run Prediction'):
-    prediction_label, confidence_scores, shap_values = make_prediction(distance, pressure, hrv,
+    prediction_label, confidence_scores, shap_values, X_test_df = make_prediction(distance, pressure, hrv,
                                                           blood_sugar, spo2, accelerometer, df)  
     max_confidence_label = max(confidence_scores, key=confidence_scores.get)
     max_confidence_score = confidence_scores[max_confidence_label]
@@ -404,23 +405,11 @@ with col3:
             ## Show output
             prediction_placeholder.write('tbd')
             
-            
-# Assuming you want to analyze the first (and only) instance in the input
-instance_index = 0
-
-# Determine the most important feature for each class
-important_features = {}
-for class_index in range(3):  # Adjust based on the number of classes
-    class_shap_values = shap_values[class_index][instance_index]
-    most_important_feature_index = np.argmax(np.abs(class_shap_values))
+    # Find the most important feature for the predicted class
+    most_important_feature_index = np.argmax(np.abs(shap_values[predicted_class_index][0]))
     most_important_feature = X_test_df.columns[most_important_feature_index]
-    important_features[f'Class {class_index}'] = most_important_feature
+    st.write(f'Most Important Feature for Predicted Class: {most_important_feature}')
 
-# Display the results
-st.write("Most important features for each class:")
-for class_label, feature in important_features.items():
-    st.write(f"{class_label}: {feature}")
-            
 
 
 
